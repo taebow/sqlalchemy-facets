4fb36f30ffdf8b0d6054cee0d903e34340bc2c94
Thibaut Frain <thibaut.frain@gmail.com>
2020-05-23 10:00:59 +0200

    subfacets tests refactoring with recursive helpers

--- tests/test_sub_facets.py
+++ tests/test_sub_facets.py
@@ -19,15 +19,14 @@ class FacetedBlog(FacetedQuery):
     )
 
 
-def check_occurences(result_container, n_facets):
+def check_children_occurence(result_container, n_facets):
     n_facets = [n_facets] if isinstance(n_facets, int) else n_facets
     assert len(result_container.facets) == n_facets[0]
     for i, facet_result in enumerate(result_container.facets):
-        print(facet_result.name)
         assert len(facet_result.buckets) > 0
         for bucket in facet_result.buckets:
             if n_facets[1:]:
-                check_occurences(bucket, n_facets[1:][i])
+                check_children_occurence(bucket, n_facets[1:][i])
 
 
 def check_types(result_container):
@@ -38,7 +37,8 @@ def check_types(result_container):
             check_types(bucket)
 
 
-def check_values(result_container, faceted_result, cond=lambda _: True):
+def check_values(result_container, faceted_result=None, cond=lambda _: True):
+    faceted_result = faceted_result or result_container
     for facet_result in result_container.facets:
         result_values = [
             getattr(r, facet_result.name)
@@ -50,7 +50,27 @@ def check_values(result_container, faceted_result, cond=lambda _: True):
             check_values(
                 bucket, faceted_result,
                 cond=lambda r: getattr(r, facet_result.name) == bucket.value
+                and cond(r)
             )
+
+
+def check_count(result_container, faceted_result=None, cond=lambda _: True):
+    faceted_result = faceted_result or result_container
+    for facet in result_container.facets:
+        result_counter = Counter(
+            getattr(r, facet.name)
+            for r in faceted_result
+            if cond(r)
+        )
+        for bucket in facet.buckets:
+            assert bucket.count == result_counter[bucket.value]
+            check_count(
+                bucket, faceted_result,
+                cond=lambda r: getattr(r, facet.name) == bucket.value
+                and cond(r)
+            )
+        assert sum(b.count for b in facet.buckets) == \
+            sum(result_counter.values())
 
 
 @pytest.mark.parametrize("query", QUERIES)
@@ -58,17 +78,8 @@ def test_return_types(query):
     faceted_query = FacetedBlog(query)
     faceted_result = faceted_query.all()
 
-    check_occurences(faceted_result, [2, [1, 1], 1])
-
-    assert len(faceted_result.facets) == 2
-    for facet in faceted_result.facets:
-        assert isinstance(facet, FacetResult)
-        for bucket in facet.buckets:
-            assert isinstance(bucket, Bucket)
-            for facet2 in bucket.facets:
-                assert isinstance(facet2, FacetResult)
-                for bucket2 in facet2.buckets:
-                    assert isinstance(bucket2, Bucket)
+    check_children_occurence(faceted_result, [2, [1, 1], 1])
+    check_types(faceted_result)
 
 
 @pytest.mark.parametrize("query", QUERIES)
@@ -76,22 +87,7 @@ def test_buckets_value(query):
     faceted_query = FacetedBlog(query)
     faceted_result = faceted_query.all()
 
-    assert len(faceted_result.facets) > 0
-    for facet in faceted_result.facets:
-        result_values = [getattr(r, facet.name) for r in faceted_result]
-
-        for bucket in facet.buckets:
-            assert bucket.value in result_values
-
-            for facet2 in bucket.facets:
-                result_values2 = [
-                    getattr(r2,  facet2.name)
-                    for r2 in faceted_result
-                    if getattr(r2, facet.name) == bucket.value
-                ]
-
-                for bucket2 in facet2.buckets:
-                    assert bucket2.value in result_values2
+    check_values(faceted_result)
 
 
 @pytest.mark.parametrize("query", QUERIES)
@@ -99,20 +95,4 @@ def test_buckets_count(query):
     faceted_query = FacetedBlog(query)
     faceted_result = faceted_query.all()
 
-    for facet in faceted_result.facets:
-        result_counter = Counter(
-            [getattr(r, facet.name) for r in faceted_result]
-        )
-        for bucket in facet.buckets:
-            assert bucket.count == result_counter[bucket.value]
-            for facet2 in bucket.facets:
-                result_counter2 = Counter([
-                    getattr(r2,  facet2.name)
-                    for r2 in faceted_result
-                    if getattr(r2, facet.name) == bucket.value
-                ])
-
-                for bucket2 in facet2.buckets:
-                    assert bucket2.count == result_counter2[bucket2.value]
-
-                assert sum(result_counter2.values()) == bucket.count
+    check_count(faceted_result)
