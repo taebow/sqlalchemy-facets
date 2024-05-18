7d1942727d9f796793fd1749718cab092353f2cf
Thibaut Frain <thibaut.frain@gmail.com>
2024-05-18 17:44:51 +0100

    Wipe everything

--- sqlalchemy_facets/__init__.py
+++
@@ -1,4 +0,0 @@
-from .api import declare_facets
-from .api import f
-from .facet import Facet
-from .facet import Mapper
--- tests/db.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sqlalchemy.ext.declarative import declarative_base
-
-conn_str = "postgresql://test:test@localhost:7777/test"
-engine = create_engine(conn_str, echo=False)
-session = Session(bind=engine)
-Base = declarative_base()
